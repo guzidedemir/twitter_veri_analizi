e07277549ee59425f87b09f6c7be8d1809f939f4
Güzide Demir <36948513+guzidedemir@users.noreply.github.com>
2018-07-18 02:49:55 +0300

    pie chart ve scatter plot taslağı

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def hello_world(word=None):
     # http://localhost:5000/
-    return render_template("content.html")
+
+    if word is None:
+        word = "Python"
+        description = "İstediğiniz kelimeleri içeren twitlerin pozitifliğini görün!"
+    else:
+        description = "Aranan kelime: {}".format(word)
+
+    tweet_data = "Twit 1\nTwit 2\nTwit 3"
+    chart_data = [['Category', 'Amount'], ['Positive', 8], ['Negative', 2]]
+    graph_data = [['Time', 'Sentiment'], ["10:54", 0.7], ["11:22", 0.5]]
+
+    return render_template("content.html", desc=description, tweet=tweet_data, chart=chart_data, graph=graph_data)
 
 
-@app.route('/search_word')
-def search_word():
-    print("Test")
-    return ""
+@app.route('/search_tweet', methods=['POST'])
+def search_tweet():
+    search_word = request.form["search_input"]
+    return hello_world(search_word)
 
 
 if __name__ == '__main__':
