2c06ad356abd52349607e2bc7bdf874f66dccd7a
myasiny <myasiny@gmail.com>
2018-07-17 15:50:22 +0300

    CSS eklendi

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     # http://localhost:5000/
-    return render_template("main.html")
+    return render_template("content.html")
 
 
 if __name__ == '__main__':
